9319bd205579bb8fafd4e99cfef18f906c37c5ef
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-03-24 19:22:31 +0300

    base commition

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$message = ""; // Your message
+$to = "";  // to whom the message
+$from = ""; // from whom the message
+$subject = ""; // Message subject
+$subject = "=?utf-8?B?".base64_encode($subject)."?=";
+$headers = "From: $from\r\n" . "Reply-to: $from\r\n" . "X-Mailer: PHP/". phpversion() . "\r\n";  // headlines
+try {
+    mail($to, $subject, $message, $headers)
+} catch (Exception $e) {
+    echo "[Error] " . $e;
+}
