2d323300483e60f560be1598d79f10c26d331ce4
pervanne69 <samvel.pogosyan2006@gmail.com>
2022-03-24 19:24:29 +0300

    second commition

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $subject = ""; // Message subject
 $subject = "=?utf-8?B?".base64_encode($subject)."?=";
 $headers = "From: $from\r\n" . "Reply-to: $from\r\n" . "X-Mailer: PHP/". phpversion() . "\r\n";  // headlines
 try {
-    mail($to, $subject, $message, $headers)
+    mail($to, $subject, $message, $headers);
 } catch (Exception $e) {
     echo "[Error] " . $e;
 }
